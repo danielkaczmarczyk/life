ef02cd91fad181089cb3c42ba80ace087b07bd66
daniel <danielkaczmarczyk2@gmail.com>
2021-02-21 17:57:04 -0800

    game now adjusts to terminal size

--- grid.py
+++ grid.py
@@ -1,11 +1,11 @@
 from cell import Cell
-
-DEBUG = False
+import os
 
 class Grid:
-    def __init__(self, w=30, h=50):
-        self.w = w
-        self.h = h
+    def __init__(self):
+        size = os.get_terminal_size() 
+        self.w = size.columns
+        self.h = size.lines
         self.grid = self.generate_grid(self.w, self.h)
 
     def get_random_cell(self):
@@ -21,14 +21,10 @@ class Grid:
 
     def print_grid(self):
         grid = self.grid
-        print("-" * (len(grid) + 2))
         for row in grid:
-            print("|", end='')
             for item in row:
                 print(item, end='')
-            print("|", end='')
             print()
-        print("-" * (len(grid) + 2))
 
     def get_cell(self, row, column):
         if row < 0 or column < 0:
@@ -41,10 +37,6 @@ class Grid:
     def check_neighbors(self, row, column):
         n = 0
         g = self.grid
-        # look at all neighbors and count
-        # a neighbor is a cell that is alive
-        # implement returning true/false based on the rules
-        # try getting a value for every direction
         offsets = []
 
         nums = [-1, 0, 1]
@@ -63,14 +55,11 @@ class Grid:
                 offsets.append([-1, nums[nums_idx]])
                 nums_idx += 1
         
-        # calculate actual offsets
-
         offsets.remove([0,0])
         for offset in offsets:
             offset[0] += row
             offset[1] += column
 
-        # check for presence of cells
         for offset in offsets:
             cell = self.get_cell(offset[0], offset[1])
             if cell:
@@ -80,40 +69,23 @@ class Grid:
         return n
 
     def update(self):
-        # get a copy of the grid
         new_grid = self.generate_grid(self.w, self.h)
-        # go over every cell in the grid
-        # count neighbors
         for row, row_array in enumerate(self.grid):
             for column, cell in enumerate(row_array):
                 cell = self.grid[row][column]
                 n = self.check_neighbors(row, column)
-                message = f"cell {row}:{column} has n of {n}"
                 if cell.alive:
-                    message += " it was found alive"
                     if n < 2:
-                        message += " and dies of underpopulation"
                         new_grid[row][column] = Cell(False)
-                        #cell.alive = False
                     elif n >= 2 and n <= 3:
-                        message += " and stays stable"
-                        #cell.alive = True
                         new_grid[row][column] = Cell(True)
                     elif n > 3:
-                        message += " and dies of overpopulation"
                         new_grid[row][column] = Cell(False)
-                        #cell.alive = False
                 else:
-                    message += " it was found dead"
                     if n == 3:
-                        message += " and will be born due to reproduction"
-                        #cell.alive = True
                         new_grid[row][column] = Cell(True)
                     else:
                         new_grid[row][column] = Cell(False)
-                if DEBUG:
-                    print(message)
 
-        # return updated copy of the grid
         self.grid = new_grid
 
---
+++ todo.md
@@ -0,0 +1,3 @@
+- [ ] fix the double loop when looking at neighbors
+- [ ] get a status bar printing at the top
+- [x] make the grid exactly the size of the terminal window on init
